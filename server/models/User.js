48449be8e5f7180a51a1498c0f52c4b2c2fd34d7
Omar-jabra <129175570+Omar-Tood@users.noreply.github.com>
2023-12-09 12:24:52 +0300

    adding comparePassword and Email Confirmation

--- server/models/User.js
+++ server/models/User.js
@@ -1,43 +1,75 @@
-import bcrypt from  'bcrypt'
-import mongoose from 'mongoose'
-import validator from 'validator'
+import bcrypt from "bcrypt";
+import mongoose from "mongoose";
+import validator from "validator";
 
-const {Schema} = mongoose
+const { Schema } = mongoose;
 
-const userSchema = new Schema({
-    email:{
-        type: String,
-        lowercase: true,
-        unique: true,
-        required: true,
-        validate: [validator.isEmail , "Please Enter a valid email"]
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      lowercase: true,
+      unique: true,
+      required: true,
+      validate: [validator.isEmail, "Please Enter a valid email"],
     },
 
-    username:{
-        type: String,
-        lowercase: true,
-        unique: true,
-        required: true,
-        validate: {
-            validator: function(value){
-                return /^[A-z][A-Za-z0-9-_]{3,23}$/.test(value)
-            },
-            message: "username does not have to be special characters"
+    username: {
+      type: String,
+      lowercase: true,
+      unique: true,
+      required: true,
+      validate: [
+        {
+          validator: function (value) {
+            return /^[A-z][A-Za-z0-9-_]{3,23}$/.test(value);
+          },
+          message: "username does not have to be special characters",
         },
+      ],
 
-        password:{
-            type: true,
-            required: true,
-            select: false,
-            validate: [
-                {
-                    validator: function(value){
-                        validator.isStrongPassword(value),
-                        message = "Password must Contain one or more alphanumeric characters and symbols";
+      password: {
+        type: true,
+        required: true,
+        select: false,
+        validate: [
+          {
+            validator: function (value) {
+              validator.isStrongPassword(value),
+                (message =
+                  "Password must Contain one or more alphanumeric characters and symbols");
+            },
+          },
+        ],
+      },
 
-                    }
-                }
-            ]
-        }
+      isEmailConfirmed: {
+        type: Boolean,
+        default: false,
+        required: true,
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
+userSchema.pre("save" , async function(next){
+    if(!this.isModified("password")){
+        return next();
     }
-})
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password,salt)
+    next();
+});
+
+userSchema.methods.comparePassword = async function(givePassword){
+  return await bcrypt.compare(givePassword , this.password)
+}
+
+const User  = mongoose.model("User" , userSchema)
+
+export default User
